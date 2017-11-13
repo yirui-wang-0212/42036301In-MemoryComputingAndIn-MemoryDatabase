b595264f8aba9940e45d22afbf6ae319350dced9
lzj <tjluozhongjin@gmail.com>
2017-11-13 11:04:56 +0800

    保留三位小数

--- InMemory/src/main/java/inmemorydemo/demo/Controller/HistoricalQuotesController.java
+++ InMemory/src/main/java/inmemorydemo/demo/Controller/HistoricalQuotesController.java
@@ -5,6 +5,7 @@ import inmemorydemo.demo.Model.HistoricalQuotesModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.Map;
 
@@ -37,15 +38,16 @@ public class HistoricalQuotesController {
         for (HistoricalQuotesModel historicalQuotesModel : historicalQuotesModelList){
             ArrayList<Object> objectArrayList = new ArrayList<>();
 //            objectArrayList.add(historicalQuotesModel.getCode());
+            DecimalFormat df = new DecimalFormat("#.000");
             objectArrayList.add(historicalQuotesModel.getTrading_day().substring(0,10));
-            objectArrayList.add(historicalQuotesModel.getOpen_value());
-            objectArrayList.add(historicalQuotesModel.getClose_value());
-            objectArrayList.add(historicalQuotesModel.getLow_value());
-            objectArrayList.add(historicalQuotesModel.getHigh_value());
-            objectArrayList.add(historicalQuotesModel.getMa5());
-            objectArrayList.add(historicalQuotesModel.getMa10());
-            objectArrayList.add(historicalQuotesModel.getMa20());
-            objectArrayList.add(historicalQuotesModel.getVolume_value());
+            objectArrayList.add(df.format(historicalQuotesModel.getOpen_value()));
+            objectArrayList.add(df.format(historicalQuotesModel.getClose_value()));
+            objectArrayList.add(df.format(historicalQuotesModel.getLow_value()));
+            objectArrayList.add(df.format(historicalQuotesModel.getHigh_value()));
+            objectArrayList.add(df.format(historicalQuotesModel.getMa5()));
+            objectArrayList.add(df.format(historicalQuotesModel.getMa10()));
+            objectArrayList.add(df.format(historicalQuotesModel.getMa20()));
+            objectArrayList.add(df.format(historicalQuotesModel.getVolume_value()));
             responseLists.add(objectArrayList);
         }
 
