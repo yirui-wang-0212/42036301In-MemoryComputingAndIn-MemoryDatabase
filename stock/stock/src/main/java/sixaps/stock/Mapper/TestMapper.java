f9ee55c45afaf828ac36b623bf01b608af4a0d30
lzj <tjluozhongjin@gmail.com>
2017-12-17 16:57:44 +0800

    修复bug

--- stock/stock/src/main/java/sixaps/stock/Mapper/TestMapper.java
+++ stock/stock/src/main/java/sixaps/stock/Mapper/TestMapper.java
@@ -2,10 +2,12 @@ package sixaps.stock.Mapper;
 
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
+import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 
 @Mapper
+@Service
 public interface TestMapper {
 
     @Select("select ID from warehouse_data_days where id = 1")
